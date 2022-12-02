6ac20ef32c5b01c733724ff57110abfe7c45d92f
Micah Olivares <micah.olivares@jortechnology.com>
2022-12-02 00:09:28 -0600

    Added Draw (to window) function

--- Pong-Python/Pong_Python.py
+++ Pong-Python/Pong_Python.py
@@ -6,11 +6,25 @@ WIDTH, HEIGHT = 700, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Pong")
 
+FPS = 60
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+
+def draw(win): 
+    win.fill(WHITE)
+    pygame.display.update()
+
 
 def main(): 
     run = True
+    clock = pygame.time.Clock()
     
     while run: 
+        clock.tick(FPS)
+        draw(WIN)
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT: 
                 run = False
